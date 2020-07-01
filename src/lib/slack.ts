7a9fc43ff4ed48bac6caccf5bb300c700e138a97
seungbin-deepnatural <67671431+seungbin-deepnatural@users.noreply.github.com>
2020-07-01 13:09:31 +0900

    Update slack.ts

--- src/lib/slack.ts
+++ src/lib/slack.ts
@@ -29,18 +29,18 @@ export default async({ weather, news, date, url }: slackArgs) => {
         short: true,
       },
       {
-        title: '🏞️ 날씨 / 부산',
+        title: '🏞️ 날씨 / 서울',
         value: weather.weather,
         short: true,
       },
       {
-        title: '🌡 온도 / 부산',
+        title: '🌡 온도 / 서울',
         value: weather.temp,
         short: true,
       },
     ],
 
-    footer: '제작: 재웜',
+    footer: '제작: 승빈',
     footer_icon: 'https://images-ext-2.discordapp.net/external/GyQicPLz_zQO15bOMtiGTtC4Kud7JjQbs1Ecuz7RrtU/https/cdn.discordapp.com/embed/avatars/1.png',
   });
 
