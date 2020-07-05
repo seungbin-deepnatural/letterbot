b10580b9cdac2d28720a550e4b77b2318a191548
seungbin-deepnatural <67671431+seungbin-deepnatural@users.noreply.github.com>
2020-07-05 15:01:27 +0900

    Update slack.ts

--- src/lib/slack.ts
+++ src/lib/slack.ts
@@ -12,8 +12,7 @@ interface slackArgs {
 }
 
 export default async({ weather, news, date, url }: slackArgs) => {
-  const todayDate = new Date()
-  const today = new Date(todayDate -540 * 60 * 1000).toLocaleDateString().replace(/\. /g, '-').replace('.', '');
+  const today = new Date(Date() -540 * 60 * 1000).toLocaleDateString().replace(/\. /g, '-').replace('.', '');
 
   let message: any = {
     attachments: [],
